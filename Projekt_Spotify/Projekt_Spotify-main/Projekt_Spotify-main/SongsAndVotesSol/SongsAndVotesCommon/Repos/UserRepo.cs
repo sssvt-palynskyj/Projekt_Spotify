2430f7e5d9c70814a2e6b2b2663760314886874f
sssvt-palynskyj <internety69g@gmail.com>
2021-02-24 10:04:29 +0100

    message

--- Projekt_Spotify/Projekt_Spotify-main/Projekt_Spotify-main/SongsAndVotesSol/SongsAndVotesCommon/Repos/UserRepo.cs
+++ Projekt_Spotify/Projekt_Spotify-main/Projekt_Spotify-main/SongsAndVotesSol/SongsAndVotesCommon/Repos/UserRepo.cs
@@ -28,39 +28,216 @@ namespace SongsAndVotesCommon.Repos
         /// <summary>Header line in the repo.</summary>
         private const string HeaderLine = "Username;Password";
 
-        public void Add(User user)
+        public bool UserExists(User user)
         {
-            throw new NotImplementedException();
+            bool x = false;
+
+            StreamReader sr = new StreamReader(@"E:\GitHub\local\Projekt\Projekt_Spotify\Projekt_Spotify-main\Projekt_Spotify-main\SongsAndVotesSol\Resources\Database\User.csv");
+            sr.ReadLine(); //aby se vynechala první lina jinak by šlo použít Username;Password jako login
+            while (!sr.EndOfStream)
+            {
+                string oneLine = sr.ReadLine();
+                string[] splitLineName = oneLine.Split(';');
+                string lineName = splitLineName[0];
+
+                if (lineName == Convert.ToString(user.Username))
+                {
+                    x = true;
+                }
+            }
+            sr.Close();
+            sr.Dispose();
+            return x;
         }
 
-        public bool Exists(User user)
+
+
+        /// <summary>
+        /// Checks with the database whether there is a given user with a given password.
+        /// </summary>
+        /// <param name="user">User to check for.</param>
+        /// <returns>Returns true :-: the given combination of username/password is valid, false :-: the username and/or password are wrong.</returns>
+        public bool CheckUsernameAndPassword(User user)
         {
-            throw new NotImplementedException();
+            bool x = false;
+            string daWay = (@"E:\GitHub\local\Projekt\Projekt_Spotify\Projekt_Spotify-main\Projekt_Spotify-main\SongsAndVotesSol\Resources\Database\User.csv");
+            StreamReader sr = new StreamReader(daWay);
+            sr.ReadLine();
+
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                string[] splitLine = line.Split(';');
+
+                string name = splitLine[0];
+                string pass = splitLine[1];
+
+                if (name == user.Username && pass == user.Password)
+                {
+                    x = true;
+                }
+            }
+            sr.Close();
+            sr.Dispose();
+            return x;
+        }
+
+        public IList<User> GetList(User user)
+        {
+            List<User> listOfUsers = new List<User>();
+            StreamReader sr = new StreamReader(@"E:\GitHub\local\Projekt\Projekt_Spotify\Projekt_Spotify-main\Projekt_Spotify-main\SongsAndVotesSol\Resources\Database\User.csv");
+
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                string[] splitLine = line.Split(';');
+                User usr = new User(splitLine[0], splitLine[1]);
+
+                listOfUsers.Add(usr);
+            }
+            sr.Close();
+            sr.Dispose();
+            return listOfUsers;
         }
 
         public IList<User> FindList(User user)
         {
-            throw new NotImplementedException();
+            List<User> listOfUsers = new List<User>();
+            StreamReader sr = new StreamReader(@"E:\GitHub\local\Projekt\Projekt_Spotify\Projekt_Spotify-main\Projekt_Spotify-main\SongsAndVotesSol\Resources\Database\User.csv");
+
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                string[] splitLine = line.Split(';');
+                User usr = new User(splitLine[0], null);
+
+                listOfUsers.Add(usr);
+            }
+            sr.Close();
+            sr.Dispose();
+            return listOfUsers;
         }
 
-        public IList<User> GetList()
+        public bool Exists(User user)
         {
-            throw new NotImplementedException();
+            bool x = false;
+
+            StreamReader sr = new StreamReader(@"E:\GitHub\local\Projekt\Projekt_Spotify\Projekt_Spotify-main\Projekt_Spotify-main\SongsAndVotesSol\Resources\Database\User.csv");
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                string oneLine = sr.ReadLine();
+                string[] splitLineName = oneLine.Split(';');
+                string lineName = splitLineName[0];
+
+                if (lineName == Convert.ToString(user.Username))
+                {
+                    x = true;
+                }
+            }
+            sr.Close();
+            sr.Dispose();
+            return x;
         }
 
         public User Load(User user)
         {
-            throw new NotImplementedException();
-        }
+            StreamReader sr = new StreamReader(@"E:\GitHub\local\Projekt\Projekt_Spotify\Projekt_Spotify-main\Projekt_Spotify-main\SongsAndVotesSol\Resources\Database\User.csv");
+            sr.ReadLine();
+            User usr = null;
 
-        public void Remove(User user)
-        {
-            throw new NotImplementedException();
+            try
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    string[] splitLine = line.Split(';');
+
+                    if (splitLine[0] == user.Username)
+                    {
+                        usr = new User(splitLine[0], splitLine[1]);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                sr.Close();
+                sr.Dispose();
+            }
+
+            if (usr != null)
+            {
+                return usr;
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
         }
 
         public void Store(User user)
         {
-            throw new NotImplementedException();
+            using (StreamWriter sw = new StreamWriter(@"E:\GitHub\local\Projekt\Projekt_Spotify\Projekt_Spotify-main\Projekt_Spotify-main\SongsAndVotesSol\Resources\Database\User.csv", true))
+            using (StreamReader sr = new StreamReader(@"E:\GitHub\local\Projekt\Projekt_Spotify\Projekt_Spotify-main\Projekt_Spotify-main\SongsAndVotesSol\Resources\Database\User.csv"))
+            using (StreamReader sr2 = new StreamReader(@"E:\GitHub\local\Projekt\Projekt_Spotify\Projekt_Spotify-main\Projekt_Spotify-main\SongsAndVotesSol\Resources\Database\User.csv"))
+            {
+                int i = 0;
+                //List<User> usercsv = new List<User>();
+                //User usr = new User(null, null);
+                string line;
+                //string[] splitLine;
+
+                while (!sr.EndOfStream)
+                {
+                    //line = sr.ReadLine();
+                    //splitLine = line.Split(';');
+                    //usr.Username = splitLine[0];
+                    //usr.Password = splitLine[1];
+
+                    //usercsv.Add(usr);
+                    i++;
+                }
+                //usercsv.Add((user));
+
+                //for (int x = 0; x <= i; x++)
+                //{
+                string writeLine = user.Username + ";" + user.Password;
+                sw.WriteLine(writeLine); //usercsv.LastIndexOf 
+                //}
+            }
+        }
+
+        public void Add(User user)
+        {
+            if (Exists(user) == false)
+            {
+                StreamWriter sw = new StreamWriter(@"E:\GitHub\local\Projekt\Projekt_Spotify\Projekt_Spotify-main\Projekt_Spotify-main\SongsAndVotesSol\Resources\Database\User.csv", );
+            }
+        }
+
+        public void Remove(User user)
+        {
+            using (StreamReader sr = new StreamReader(@"E:\GitHub\local\Projekt\Projekt_Spotify\Projekt_Spotify-main\Projekt_Spotify-main\SongsAndVotesSol\Resources\Database\User.csv"))
+            {
+                //string tempFile = Path.GetTempFileName();
+                string readLine;
+                string delLine = user.Username + ";" + user.Password;
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    readLine = sr.ReadLine();
+                    if (readLine == delLine)
+                    {
+                        //
+                    }
+                }
+            }
         }
     }
 
--- Projekt_Spotify/Projekt_Spotify-main/Projekt_Spotify-main/SongsAndVotesSol/SongsAndVotesCommon/Services/UserService.cs
+++ Projekt_Spotify/Projekt_Spotify-main/Projekt_Spotify-main/SongsAndVotesSol/SongsAndVotesCommon/Services/UserService.cs
@@ -3,6 +3,7 @@
 using SongsAndVotesCommon.BusinessObjects;
 using SongsAndVotesCommon.Interfaces;
 using SongsAndVotesCommon.Factories;
+using SongsAndVotesCommon.Repos;
 
 using System.IO;
 using System.Collections.Generic;
@@ -15,7 +16,7 @@ namespace SongsAndVotesCommon.Services
     /// <summary>
     /// Provides API methods related to the user management, login, logout etc.
     /// </summary>
-    public class UserService : IUserRepo
+    public class UserService
     {
 
 
@@ -32,6 +33,22 @@ namespace SongsAndVotesCommon.Services
         {
             this.userRepo = UserRepoFactory.GetUserRepo();
         }
+        public bool UserExists(User user)
+        {
+            return UserExists(user);
+        }
+        public bool CheckUsernameAndPassword(User user)
+        {
+            return UserExists(user);
+        }
+        public List<User> GetList(User user)
+        {
+            return GetList(user);
+        }
+        public List<User> asd()
+        {
+            re
+        }
 
 
 
@@ -40,215 +57,7 @@ namespace SongsAndVotesCommon.Services
         /// </summary>
         /// <param name="user">User to check for.</param>
         /// <returns>Returns true :-: a user with the given username exists, false :-: no such user.</returns>
-        public bool UserExists(User user)
-        {
-            bool x = false;
 
-            StreamReader sr = new StreamReader(@"E:\GitHub\local\Projekt\Projekt_Spotify\Projekt_Spotify-main\Projekt_Spotify-main\SongsAndVotesSol\Resources\Database\User.csv");
-            sr.ReadLine(); //aby se vynechala první lina jinak by šlo použít Username;Password jako login
-            while (!sr.EndOfStream)
-            {
-                string oneLine = sr.ReadLine();
-                string[] splitLineName = oneLine.Split(';');
-                string lineName = splitLineName[0];
-
-                if(lineName == Convert.ToString(user.Username))
-                {
-                    x = true;
-                }
-            }
-            sr.Close();
-            sr.Dispose();
-            return x;
-        }
-
-
-
-        /// <summary>
-        /// Checks with the database whether there is a given user with a given password.
-        /// </summary>
-        /// <param name="user">User to check for.</param>
-        /// <returns>Returns true :-: the given combination of username/password is valid, false :-: the username and/or password are wrong.</returns>
-        public bool CheckUsernameAndPassword(User user)
-        {
-            bool x = false;
-            string daWay = (@"E:\GitHub\local\Projekt\Projekt_Spotify\Projekt_Spotify-main\Projekt_Spotify-main\SongsAndVotesSol\Resources\Database\User.csv");
-            StreamReader sr = new StreamReader(daWay);
-            sr.ReadLine();
-
-            while (!sr.EndOfStream)
-            {
-                string line = sr.ReadLine();
-                string[] splitLine = line.Split(';');
-
-                string name = splitLine[0];
-                string pass = splitLine[1];
-
-                if(name == user.Username && pass == user.Password)
-                {
-                    x = true;
-                }
-            }
-            sr.Close();
-            sr.Dispose();
-            return x;
-        }
-
-        public IList<User> GetList(User user)
-        {
-            List<User> listOfUsers = new List<User>();
-            StreamReader sr = new StreamReader(@"E:\GitHub\local\Projekt\Projekt_Spotify\Projekt_Spotify-main\Projekt_Spotify-main\SongsAndVotesSol\Resources\Database\User.csv");
-
-            sr.ReadLine();
-            while (!sr.EndOfStream)
-            {
-                string line = sr.ReadLine();
-                string[] splitLine = line.Split(';');
-                User usr = new User(splitLine[0], splitLine[1]);
-                //usr.Username = splitLine[0];
-                //usr.Password = splitLine[1];
-
-                listOfUsers.Add(usr);
-            }
-            sr.Close();
-            sr.Dispose();
-            return listOfUsers;
-        }
-
-        public IList<User> FindList(User user)
-        {
-            List<User> listOfUsers = new List<User>();
-            StreamReader sr = new StreamReader(@"E:\GitHub\local\Projekt\Projekt_Spotify\Projekt_Spotify-main\Projekt_Spotify-main\SongsAndVotesSol\Resources\Database\User.csv");
-
-            sr.ReadLine();
-            while (!sr.EndOfStream)
-            {
-                string line = sr.ReadLine();
-                string[] splitLine = line.Split(';');
-                User usr = new User(splitLine[0], null);
-
-                listOfUsers.Add(usr);
-            }
-            sr.Close();
-            sr.Dispose();
-            return listOfUsers;
-        }
-
-        public bool Exists(User user)
-        {
-            bool x = false;
-
-            StreamReader sr = new StreamReader(@"E:\GitHub\local\Projekt\Projekt_Spotify\Projekt_Spotify-main\Projekt_Spotify-main\SongsAndVotesSol\Resources\Database\User.csv");
-            sr.ReadLine();
-            while (!sr.EndOfStream)
-            {
-                string oneLine = sr.ReadLine();
-                string[] splitLineName = oneLine.Split(';');
-                string lineName = splitLineName[0];
-
-                if (lineName == Convert.ToString(user.Username))
-                {
-                    x = true;
-                }
-            }
-            sr.Close();
-            sr.Dispose();
-            return x;
-        }
-
-        public User Load(User user)
-        {
-            StreamReader sr = new StreamReader(@"E:\GitHub\local\Projekt\Projekt_Spotify\Projekt_Spotify-main\Projekt_Spotify-main\SongsAndVotesSol\Resources\Database\User.csv");
-            sr.ReadLine();
-            User usr = null;
-
-            try
-            {
-                while (!sr.EndOfStream)
-                {
-                    string line = sr.ReadLine();
-                    string[] splitLine = line.Split(';');
-                    
-                    if(splitLine[0] == user.Username)
-                    {
-                        usr = new User(splitLine[0], splitLine[1]);
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            finally
-            {
-                sr.Close();
-                sr.Dispose();
-            }
-
-            if(usr != null)
-            {
-                return usr;
-            }
-            else
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        public void Store(User user)
-        {
-            using(StreamWriter sw = new StreamWriter(@"E:\GitHub\local\Projekt\Projekt_Spotify\Projekt_Spotify-main\Projekt_Spotify-main\SongsAndVotesSol\Resources\Database\User.csv"))
-            using(StreamReader sr = new StreamReader(@"E:\GitHub\local\Projekt\Projekt_Spotify\Projekt_Spotify-main\Projekt_Spotify-main\SongsAndVotesSol\Resources\Database\User.csv"))
-            using(StreamReader sr2 = new StreamReader(@"E:\GitHub\local\Projekt\Projekt_Spotify\Projekt_Spotify-main\Projekt_Spotify-main\SongsAndVotesSol\Resources\Database\User.csv"))
-            {
-                int i = 0;
-                List<User> usercsv = new List<User>();
-                User usr = new User(null, null);
-                string line;
-                string[] splitLine;
-
-                while (!sr.EndOfStream)
-                {
-                    line = sr.ReadLine();
-                    splitLine = line.Split(';');
-                    usr.Username = splitLine[0];
-                    usr.Password = splitLine[1];
-
-                    usercsv.Add(usr);
-                    i++;
-                }
-                usercsv.Add((user));
-
-                for (int x = 0; x <= i; x++)
-                {
-                    // sw.WriteLine(usercsv.LastIndexOf)
-                }
-            }
-        }
-
-        public void Add(User user)
-        {
-            
-        }
-
-        public void Remove(User user)
-        {
-            using (StreamReader sr = new StreamReader(@"E:\GitHub\local\Projekt\Projekt_Spotify\Projekt_Spotify-main\Projekt_Spotify-main\SongsAndVotesSol\Resources\Database\User.csv"))
-            {
-                //string tempFile = Path.GetTempFileName();
-                string readLine;
-                string delLine = user.Username + ";" + user.Password;
-                sr.ReadLine();
-                while(!sr.EndOfStream)
-                {
-                    readLine = sr.ReadLine();
-                    if(readLine == delLine)
-                    {
-                        //
-                    }
-                }
-            }
-        }
     }
 
 
